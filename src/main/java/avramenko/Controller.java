2fe55a3762cab006d8b9056ccfbfbc62453ed0f2
Avramenko Kateryna <k.avramenko111@gmail.com>
2018-10-04 07:20:20 +0300

    Fixed output

--- src/main/java/avramenko/Controller.java
+++ src/main/java/avramenko/Controller.java
@@ -3,11 +3,12 @@ package main.java.avramenko;
 public class Controller {
     public static void main(String[] args) {
         View view = new View();
+        System.out.println("Developer: Avramenko K.A.\nEnter matrix dimension");
         int n = view.readN();
         long[][] matrix = Service.generateRandomArray(n);
         float[][] finalMatrix = Service.matrixChange(matrix);
-        System.out.println("Developer: Kateryna Avramenko/n Enter matrix dimension/n");
         view.printMatrix(matrix);
+        System.out.println();
         view.printMatrix(finalMatrix);
     }
 }
--- src/main/java/avramenko/View.java
+++ src/main/java/avramenko/View.java
@@ -12,7 +12,7 @@ public class View {
     public void printMatrix(long[][] matrix) {
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix.length; j++){
-                System.out.printf(" %d ", matrix[i][j]);
+                System.out.printf("%4d ", matrix[i][j]);
             }
             System.out.println();
         }
@@ -21,7 +21,7 @@ public class View {
     public void printMatrix(float[][] finalMatrix) {
         for(int i=0; i<finalMatrix.length; i++){
             for(int j=0; j<finalMatrix.length; j++){
-                System.out.printf(" %f ", finalMatrix[i][j]);
+                System.out.printf("%10f ", finalMatrix[i][j]);
             }
             System.out.println();
         }
